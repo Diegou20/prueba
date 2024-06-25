880d73547e4eb54b3da7dd42f84cba8e5b59abec
Diegou20 <die.sepulvedah@duocuc.cl>
2024-06-25 17:37:47 -0400

    prueba.py

---
+++ prueba.py
@@ -0,0 +1,35 @@
+import _json
+
+
+
+
+
+   
+print("***************") 
+print("* MUNDO LIBRO *")
+print("***************")
+while True:
+ 
+  print("1.-mantenedor de usuarios")
+  print("2.-reportes")
+  print("3.-salir")
+ 
+  opc=int(input("elija una de las opciones:"))
+ 
+  if opc==1:  
+     print("****mantenedor de usuarios****")
+     print("1.-agregar usuario")
+     print("2.-editar usuario ") 
+     print("3.-eliminar usuario")
+     print("4.-buscar usuario ") 
+     print("5.-volver")
+  elif opc==2:
+     print("reportes")
+ 
+  elif opc==3:
+     print("saliendo...")
+     break
+
+  
+  
+
